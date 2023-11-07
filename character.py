ca1b7265f30099b57422c1683b594bee7ae0f89b
tedirland <irlandth@gmail.com>
2023-11-07 08:20:29 -0600

    upload assets+ logic for character mvmt

--- character.py
+++ character.py
@@ -1,10 +1,24 @@
+import math
 import pygame
 from constants import RED
 
 class Character():
-    def __init__(self,x,y ):
+    def __init__(self,x, y, image ):
+        self.image = image
         self.rect = pygame.Rect(0,0,40,40)
         self.rect.center = (x,y)
     
+    def move(self, dx, dy ):
+
+        # control diagonal speed
+        if dx != 0 and dy != 0:
+            dx = dx * (math.sqrt(2) /2)
+            dy = dy *(math.sqrt(2) /2)
+
+        self.rect.x += dx
+        self.rect.y += dy
+
     def draw(self, surface):
-        pygame.draw.rect(surface,RED, self.rect)
+        surface.blit(self.image, self.rect)
+        pygame.draw.rect(surface,RED, self.rect, 1)
+    
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, BG, FPS, PLAYER_SPEED
 from character import Character
 
 pygame.init()
@@ -11,20 +11,75 @@ pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Dungeon of Darkness")
 
-# create player
+# create clock for maintaining frame rate
 
-player = Character(100,100)
+clock = pygame.time.Clock()
+
+# define player movement vars
+moving_left = False
+moving_right = False
+moving_up = False
+moving_down = False
+
+player_image = pygame.image.load("assets/images/characters/elf/idle/0.png").convert_alpha()
+
+# create player
+player = Character(100,100, player_image)
 
 
 # main game loop
 run = True
 while run:
+
+    # control frame rate
+    clock.tick(FPS)
+
+    screen.fill(BG)
+    # calculate player movement
+    dx = 0
+    dy = 0
+    if moving_right:
+        dx = PLAYER_SPEED
+    if moving_left:
+        dx = -PLAYER_SPEED
+    if moving_up:
+        dy = -PLAYER_SPEED
+    if moving_down:
+        dy = PLAYER_SPEED
+    
+
+    # move player
+    player.move(dx,dy)
     
     player.draw(screen)
     #event handler
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+        # take keyboard presses
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                moving_left = True
+            if event.key == pygame.K_d:
+                moving_right = True
+            if event.key == pygame.K_w:
+                moving_up = True
+            if event.key == pygame.K_s:
+                moving_down = True
+
+                # take keyboard button released
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                moving_left = False
+            if event.key == pygame.K_d:
+                moving_right = False
+            if event.key == pygame.K_w:
+                moving_up = False
+            if event.key == pygame.K_s:
+                moving_down = False
+            
+        
+
     
     pygame.display.update()
 
